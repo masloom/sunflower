d1f7dcfe88888f5e7bf2d455c94031dbb5b9230f
masloom <97346687+masloom@users.noreply.github.com>
2022-01-08 22:21:43 +0800

    init

---
+++ precache-manifest.c0a388fccac74b4562c4467ca67f78ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6cdaaebd1b5d285fc06228bf0da331",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80c09f970f21d11c8d84",
+    "url": "./static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "a213318b967bd5878ea4",
+    "url": "./static/css/main.a39b6b7c.chunk.css"
+  },
+  {
+    "revision": "80c09f970f21d11c8d84",
+    "url": "./static/js/2.0c08fb64.chunk.js"
+  },
+  {
+    "revision": "dbd161b9ac48e6187d724e7ff9d3abb2",
+    "url": "./static/js/2.0c08fb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a213318b967bd5878ea4",
+    "url": "./static/js/main.2e44c54f.chunk.js"
+  },
+  {
+    "revision": "f3e243698a43d932e3f0",
+    "url": "./static/js/runtime-main.c7647ef6.js"
+  },
+  {
+    "revision": "51c1242870ecfc1e74411c95c4d89904",
+    "url": "./static/media/logo.51c12428.png"
+  },
+  {
+    "revision": "afdb2d67b2cfa0dad4d27d0b71728a04",
+    "url": "./static/media/speed-up.afdb2d67.png"
+  },
+  {
+    "revision": "9a61915e7b3cdcf7f844ce053c67101d",
+    "url": "./static/media/spinning_coin.9a61915e.gif"
+  },
+  {
+    "revision": "70fb56907514e7f4469ead631eba45c9",
+    "url": "./static/media/willow_tree.70fb5690.mp3"
+  }
+]);
